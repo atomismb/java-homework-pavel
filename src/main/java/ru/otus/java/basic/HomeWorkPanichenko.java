adcbabba3911a77e6e395aaed414a35590218960
PavelPanichenko_work <atomismb@yandex.ru>
2025-08-12 22:34:45 +0300

    test push from work laptop

--- src/main/java/ru/otus/java/basic/HomeWorkPanichenko.java
+++ src/main/java/ru/otus/java/basic/HomeWorkPanichenko.java
@@ -2,7 +2,7 @@ package ru.otus.java.basic;
 
 public class HomeWorkPanichenko {
     public static void main(String[] args) {
-        System.out.println("Hello World Again");
+        System.out.println("Hello World Again!");
 
     }
 }
