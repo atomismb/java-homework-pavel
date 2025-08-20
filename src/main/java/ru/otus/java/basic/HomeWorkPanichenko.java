24b5a32d5249fde6b56ac8eb27e230a5f686f597
PavelPanichenko_work <atomismb@yandex.ru>
2025-08-20 14:34:16 +0300

    homework 2 (3 tasks)

--- src/main/java/ru/otus/java/basic/HomeWorkPanichenko.java
+++ src/main/java/ru/otus/java/basic/HomeWorkPanichenko.java
@@ -1,108 +1,41 @@
 package ru.otus.java.basic;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HomeWorkPanichenko {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число от 1 до 5 для выбора метода");
-        while (true) {
+        //repeatRow(5,"Hello OTUS!");
+        //sumArray(new int[] {4,6,1,3,4,8,7,10});
+        fillArray(200, new int[10]);
+    }
 
-            int methodNum = scanner.nextInt();
+    public static void repeatRow(int rowNumber, String row) {
+        for (int i = 0; i < rowNumber; i++) {
+            System.out.println(row);
+        }
+    }
 
-            if (methodNum == 1) {
-                System.out.println("Метод greetings()");
-                greetings();
-            } else if (methodNum == 2) {
+    public static void sumArray(int[] array1) {
+        int result = 0;
+        for (int i = 0; i < array1.length; i++) {
 
-                int a = -(int) (Math.random() * 100 + 1);
-                int b = (int) (Math.random() * 10 + 1);
-                int c = (int) (Math.random() * 100 + 1);
-                System.out.println("Метод checkSign()");
-                System.out.println("Выводится знак результата суммирования чисел а = " + a + "; b = " + b + "; c = " + c + ";");
-
-                checkSign(a, b, c);
-            } else if (methodNum == 3) {
-                System.out.println("Метод selectColor()");
-                selectColor();
-            } else if (methodNum == 4) {
-                System.out.println("Метод compareNumbers()");
-                compareNumbers();
-            } else if (methodNum == 5) {
-
-                int percent = 50;
-                int range = (int) (Math.random() * 100 + 1);
-                boolean increment = range > percent;
-                int initValue = (int) (Math.random() * 100 + 1);
-                int delta = (int) (Math.random() * 10 + 1);
-
-                System.out.println("Метод addOrSubtractAndPrint()");
-                if (increment) {
-                    System.out.println("Инкремент положительный");
-                } else {
-                    System.out.println("Инкремент отрицательный");
-                }
-                System.out.println("Изначальное число = " + initValue + "; Дельта = " + delta);
-                addOrSubtractAndPrint(initValue, delta, increment);
-
-            } else {
-                System.out.println("Вы ввели недопустимое число");
-                System.out.println("Введите число из диапазона 1-5");
-
-
+            if (array1[i] > 5) {
+                result += array1[i];
             }
         }
-
+        System.out.println(result);
     }
 
-
-    public static void greetings() {
-        System.out.println("Hello");
-        System.out.println("World");
-        System.out.println("from");
-        System.out.println("Java");
-    }
-
-    public static void checkSign(int a, int b, int c) {
-        int result = a + b + c;
-        if (result > 0) {
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
+    public static void fillArray(int number, int[] array2) {
+        for (int i = 0; i < array2.length; i++) {
+            array2[i] = number;
         }
+        System.out.println(Arrays.toString(array2));
     }
 
-    public static void selectColor() {
-        int data = 99999;
-        if (data <= 10) {
-            System.out.println("Красный");
-        } else if (data <= 20) {
-            System.out.println("Желтый");
-        } else {
-            System.out.println("Зеленый");
-        }
-    }
 
-    public static void compareNumbers() {
-        int a = 10;
-        int b = -10;
-        if (a >= b) {
-            System.out.println("a>=b");
-        } else {
-            System.out.println("a<b");
-        }
-
-    }
-
-    public static void addOrSubtractAndPrint(int initValue, int delta, boolean increment) {
-        if (increment) {
-            int result = initValue + delta;
-            System.out.println("Результат сложения = " + result);
-        } else {
-            int result = initValue - delta;
-            System.out.println("Результат вычитания = " + result);
-        }
-    }
 }
 
 
