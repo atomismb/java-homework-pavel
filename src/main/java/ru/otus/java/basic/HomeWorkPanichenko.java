337ee9c0e2f73b6df7ae0170667028256ff3f4e3
p.panichenko <atomismb@yandex.ru>
2025-10-07 20:37:26 +0300

    homework 9 save

--- src/main/java/ru/otus/java/basic/HomeWorkPanichenko.java
+++ src/main/java/ru/otus/java/basic/HomeWorkPanichenko.java
@@ -1,8 +1,31 @@
 package ru.otus.java.basic;
 
+import java.util.ArrayList;
+
 public class HomeWorkPanichenko {
     public static void main(String[] args) {
-        System.out.println("Hello World Again!");
+        //System.out.println(minMaxList(10,1));
+        sumOverFive(minMaxList(10,1));
 
+    }
+    public static ArrayList minMaxList(int max, int min){
+        ArrayList list = new ArrayList<>();
+        for (int i = min; i <= max; i++) {
+            list.add(String.valueOf(min));
+            min++;
+        }
+        return list;
+    }
+    public static int sumOverFive (ArrayList list){
+        int result = 0;
+        for (int i = 0; i < list.size(); i++) {
+            String number = (String) list.get(i);
+            number
+             if (number > 5){
+                result += number;
+            }
+
+        }
+        return result;
     }
 }
